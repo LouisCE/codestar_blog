4ad2db7fd9daf66bf659159c99a8fb5943e423fd
LouisCE <louiscowell190@yahoo.co.uk>
2025-10-29 13:15:46 +0000

    Secure SECRET_KEY and confirm database connection

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-psc%3-kjhvs2+=@e3b$de(3n$-b28k4$!p78^t@qv+$efjpen4'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
